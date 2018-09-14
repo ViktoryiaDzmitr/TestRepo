5cafc7d8bd6e414a96a1b826a93ff197850b497f
Viktroyia Dzmitrachonak <viktoryia.dzmitrachonak@leverx.com>
2018-09-14 14:39:43 +0300

    Third commit

--- git/Program.cs
+++ git/Program.cs
@@ -11,7 +11,7 @@ namespace Calculator
       
         static void Main(string[] args)
         {
-            Console.WriteLine("Second commit");
+            Console.WriteLine("Third commit");
             Console.WriteLine("-----------------------");
             Console.WriteLine("|Operators: +, -, *, /|");
             Console.WriteLine("| c - previous result |");
